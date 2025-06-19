b25238e0be1f515a6940821090ae6fedbaf80f90
Ahmedhatem307 <84460531+Ahmedhatem307@users.noreply.github.com>
2025-06-19 14:25:51 +0300

    Add hamburger menu for mobile devices in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,16 @@
   <header>
     <nav class="navBar">
       <div class="navBar__leftSide">
+        <div class="navBar__hamburger" @click="toggleMenu">☰</div>
         <img class="navBar__leftSide--logo" src="../assets/images/logo.png" />
-        <router-link to="/products" class="navBar__leftSide--link"
-          >Products</router-link
-        >
-        <router-link to="/contact" class="navBar__leftSide--link"
-          >Contact</router-link
-        >
+        <div class="navBar__links" :class="{ open: isMenuOpen }">
+          <router-link to="/products" class="navBar__leftSide--link"
+            >Products</router-link
+          >
+          <router-link to="/contact" class="navBar__leftSide--link"
+            >Contact</router-link
+          >
+        </div>
       </div>
 
       <div class="navBar__rightSide">
@@ -17,6 +20,24 @@
     </nav>
   </header>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+    closeMenu() {
+      this.isMenuOpen = false;
+    },
+  },
+};
+</script>
 
 <style scoped>
 .navBar {
@@ -48,9 +69,45 @@
     width: auto;
     height: 60px;
   }
+
+  & .navBar__links {
+    display: flex;
+    gap: 1rem;
+  }
+}
+
+.navBar__hamburger {
+  display: none;
+  font-size: 2rem;
+  cursor: pointer;
 }
 .router-link-active {
   font-weight: bold;
   text-decoration: none;
+}
+
+@media (max-width: 768px) {
+  .navBar__hamburger {
+    display: block;
+  }
+
+  .navBar__links {
+    display: none !important;
+    flex-direction: column;
+    position: absolute;
+    top: 60px;
+    padding: 1rem;
+    z-index: 1000;
+    background-color: #1c2e4a;
+
+    & .navBar__leftSide--link:hover {
+      background: rgba(255, 255, 255, 0.1);
+      transform: translateX(5px);
+    }
+  }
+
+  .navBar__links.open {
+    display: flex !important;
+  }
 }
 </style>

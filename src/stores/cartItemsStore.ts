ff23cdfdec8130ce363b763e097a4982df04404a
Ahmedhatem307 <84460531+Ahmedhatem307@users.noreply.github.com>
2025-07-08 15:50:41 +0300

    refactor: remove old vuex code with it's comments from cartItemsStore, productStore, and SelectedProductStore

--- src/stores/cartItemsStore.ts
+++ src/stores/cartItemsStore.ts
@@ -2,10 +2,6 @@ import type { CartItem } from "../types/CartItem";
 import { defineStore } from "pinia";
 import type { Product } from "../types/product";
 
-export interface cartState {
-  items: CartItem[];
-}
-
 export const useCartItemStore = defineStore("cartItem", {
   state: () => ({
     items: [] as CartItem[],
@@ -28,77 +24,24 @@ export const useCartItemStore = defineStore("cartItem", {
       if (existingItem) {
         existingItem.quantity += 1;
       } else {
-        const cartItem: CartItem = { ...product, quantity: 1 }
-        this.cartItems.push(cartItem)
+        const cartItem: CartItem = { ...product, quantity: 1 };
+        this.cartItems.push(cartItem);
       }
     },
     increaseQuantity(id: number) {
-      const item = this.items.find(i => i.id === id)
+      const item = this.items.find((i) => i.id === id);
       if (item) {
-        item.quantity += 1
+        item.quantity += 1;
       }
     },
     decreaseQuantity(id: number) {
-      const item = this.items.find(i => i.id === id)
+      const item = this.items.find((i) => i.id === id);
       if (item && item.quantity > 1) {
-        item.quantity -= 1
+        item.quantity -= 1;
       } else if (item?.quantity === 1) {
-        const index = this.items.findIndex(i => i.id === id)
-        this.items.splice(index, 1)
+        const index = this.items.findIndex((i) => i.id === id);
+        this.items.splice(index, 1);
       }
-    }
+    },
   },
 });
-
-// const cartItemsModule = {
-//     namespaced: true,
-
-//     state: ()  => ({
-//         items: []
-//     }),
-//     mutations:{
-//         ADD_ITEM(state: cartState, item: CartItem) {
-//             const existingItem = state.items.find(i => i.id === item.id)
-//             if(existingItem){
-//                 existingItem.quantity += 1
-//             }
-//             else{
-//                 state.items.push(item)
-//                 item.quantity=1
-//             }
-//         },
-//         increaseQuantity(state: cartState, id:number){
-//             const item = state.items.find(i => i.id === id)
-//             if(item){
-//                 item.quantity++
-//             }
-//         },
-//         decreaseQuantity(state: cartState, id:number){
-//             const item = state.items.find(i => i.id === id)
-//             if(item && item.quantity > 1){
-//                 item.quantity--
-//             }
-//             else if(item?.quantity === 1){
-//                 const index = state.items.findIndex(item => item.id === id)
-//                 state.items.splice(index, 1)
-//             }
-//         },
-//     },
-//     actions: {
-
-//     },
-//     getters: {
-//         cartItems: (state: cartState) => state.items,
-//         totalPrice(state: cartState){
-//             return state.items.reduce((total,item) => total + item.price * item.quantity,0)
-//         },
-//         itemCount(state: cartState){
-//             return state.items.reduce((sum,item) => sum + item.quantity,0)
-//         }
-//     },
-//     modules: {
-
-//     }
-// }
-
-// export default cartItemsModule;
--- src/stores/selectedProductStore.ts
+++ src/stores/selectedProductStore.ts
@@ -1,45 +1,18 @@
-import type { Product } from "../types/product"
-import {defineStore} from 'pinia'
-export interface SelectedProductState {
-    item: Product | null
-}
+import type { Product } from "../types/product";
+import { defineStore } from "pinia";
 
-export const useSelectedProductStore = defineStore('selectedProduct', {
+export const useSelectedProductStore = defineStore("selectedProduct", {
   state: () => ({
-    item: null as Product | null
+    item: null as Product | null,
   }),
 
   getters: {
-    getSelectedProduct: (state) => state.item
+    getSelectedProduct: (state) => state.item,
   },
 
   actions: {
     setSelectedProduct(product: Product) {
-      this.item = product
-    }
-  }
-})
-// const selectedProductModule = {
-//     namespaced: true,
-//     state: () => ({
-//         item: null
-//     }),
-//     mutations: {
-//         SET_SELECTED_PRODUCT(state: SelectedProductState, product: Product) {
-//             state.item = product
-//         }
-//     },
-//     actions: {
-//         selectProduct({commit}: any, product: Product){
-//             commit('SET_SELECTED_PRODUCT', product)
-//         }
-//     },
-//     getters: {
-//         getSelectedProduct(state: SelectedProductState){
-//             return state.item
-//         }
-//     }
-
-// }
-
-// export default selectedProductModule
+      this.item = product;
+    },
+  },
+});
